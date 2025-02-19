3e1e7c2978daf40bef833fad8de1cec86ec1074e
LAB14_USER ON WS17 <L14X08W17@citulab.net>
2025-02-19 08:19:06 +0800

    initial spring boot oauth2login

---
+++ g3-cabarrubias-karen/oauth2login/src/test/java/com/cabarrubias/oauth2login/OAuth2LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabarrubias.oauth2login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
